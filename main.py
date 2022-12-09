0b5e562bb8b0b323425c7e50818479f3d7ec1078
Rodney Tayebwa <rodneytayebwa@gmail.com>
2022-12-09 18:02:32 -0500

    Docker Assignment

---
+++ main.py
@@ -0,0 +1,31 @@
+import random
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def play():
+    user = input("What's your choice? 'r' for rock, 'p' for paper, 's' for scissors\n")
+    computer = random.choice(['r', 'p', 's'])
+
+    if user == computer:
+        return 'It\' a tie'
+
+    # r > s, s > p, p > r
+    if is_win(user, computer):
+        return 'You won!'
+
+    return 'You lost!'
+
+
+def is_win(player, opponent):
+    # return true if player wins
+    # r > s, s > p, p > r
+    if (player == 'r' and opponent == 's') or (player == 's' and opponent == 'p') or (
+            player == 'p' and opponent == 'r'):
+        return True
+
+
+playGame = play()
+print(playGame)
